178f45d800b0f3485157480024dfceafe7f507f8
Christopher Dorst <chris@dorst.email>
2017-12-12 19:09:59 -0600

    Adding EntityFrameworkTests.Run in-memory SqlLite implementation

---
+++ DevOps.Tests.EntityFramework/EntityFrameworkTests.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Data.Common;
+
+namespace DevOps.Tests.EntityFramework
+{
+    public static class EntityFrameworkTests
+    {
+        public static void Run<TDbContext>(Action<TDbContext> doWork, Action<TDbContext> verifyResults)
+            where TDbContext : DbContext
+        {
+            // https://docs.microsoft.com/en-us/ef/core/miscellaneous/testing/sqlite
+            using (var connection = new SqliteConnection("DataSource=:memory:"))
+            {
+                connection.Open();
+                var options = GetOptions(connection);
+                using (var context = GetContext(options)) context.Database.EnsureCreated();
+                using (var context = GetContext(options)) doWork(context);
+                using (var context = GetContext(options)) verifyResults(context);
+            }
+
+            TDbContext GetContext(DbContextOptions<TDbContext> options)
+                => Activator.CreateInstance(typeof(TDbContext), options) as TDbContext;
+
+            DbContextOptions<TDbContext> GetOptions(DbConnection connection)
+                => new DbContextOptionsBuilder<TDbContext>()
+                    .UseSqlite(connection)
+                    .Options;
+        }
+    }
+}
